a67ec94deb20155736cd128e094420595c1d762a
Jarred Witt <me@jarredwitt.com>
2016-11-26 14:29:18 -0600

    Added members and vehicles reducers.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 import { NavigationReducer } from '@exponent/ex-navigation';
 
 import application from './application';
+import members from './members';
+import vehicles from './vehicles';
 
 export default combineReducers({
   application,
+  members,
   navigation: NavigationReducer,
+  vehicles,
 });
