a48654b05321c71ee386a9deaa6612c129e8fd1b
Jarred Witt <me@jarredwitt.com>
2016-11-30 13:00:22 -0600

    Updated prod store

--- app/store/store.prod.js
+++ app/store/store.prod.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { createNavigationEnabledStore } from '@exponent/ex-navigation';
+import { enableBatching } from 'redux-batched-actions';
+import reduxThunk from 'redux-thunk';
 
 import rootReducer from 'reducers';
 
-export default () => {
-  const store = createStore(rootReducer);
+const createStoreWithNavigation = createNavigationEnabledStore({
+  createStore,
+  navigationStateKey: 'navigation',
+});
+
+export default (initialState = {}) => {
+  const store = createStoreWithNavigation(enableBatching(rootReducer), initialState, applyMiddleware(reduxThunk));
 
   return store;
 };
